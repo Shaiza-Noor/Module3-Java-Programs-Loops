3574d243be9360d7de2fbd02c4bc4e758ee74414
Shaiza-Noor <shaizanoor988@gmail.com>
2025-10-04 13:11:44 +0500

    Add MultiplicationTable program using for loop and user input

---
+++ MultiplicationTable.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner input=new Scanner (System.in);
+        System.out.print("Enter a number for Multiplication Table:");
+        int num=input.nextInt();
+        for(int i=1;i<=10;i++)
+        System.out.println(num +"*"+ i +"=" + num*i);
+        input.close();
+
+}
+
+    
+}
